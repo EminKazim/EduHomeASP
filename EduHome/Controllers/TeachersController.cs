1a55fa93f030593be4faeeed74264c74fe739f59
Tahmin Fatiyev <tahmin.fatiyev@gmail.com>
2022-04-18 00:58:03 +0400

    Add project files.

---
+++ EduHome/Controllers/TeachersController.cs
@@ -0,0 +1,38 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Service.Interfaces;
+using System.Threading.Tasks;
+
+namespace EduHome.Controllers
+{
+    public class TeachersController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _env;
+        private readonly ITeacherService _teacherService;
+        public TeachersController(AppDbContext context,
+                                  IWebHostEnvironment env,
+                                  ITeacherService teacherService)
+        {
+            _context = context;
+            _env = env;
+            _teacherService = teacherService;
+        }
+
+        public async Task<IActionResult> Index(int take = 3, int page = 1)
+        {
+            ViewData["Take"] = take;
+            var paginatedTeacher = await _teacherService.GetTeachers(take, page);
+            if (paginatedTeacher == null) return NotFound();
+            return View(paginatedTeacher);
+        }
+        public async Task<IActionResult> Details(int id)
+        {
+            if (id == 0) return NotFound();
+            var teacher = await _teacherService.GetTeacherDetailsById(id);
+            if (teacher is null) return NotFound();
+            return View(teacher);
+        }
+    }
+}
---
+++ EduHome/Startup.cs
@@ -0,0 +1,66 @@
+using Domain;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Service;
+
+namespace EduHome
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddServiceLayer();
+            services.AddDomainLayer(Configuration);
+            services.AddControllersWithViews();
+            services.AddDbContext<AppDbContext>();
+            //services.AddDbContext<AppDbContext>(options =>
+            //{
+            //    options.UseSqlServer("name=ConnectionStrings:EduHomeConnection");
+            //});
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                name: "areas",
+                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
